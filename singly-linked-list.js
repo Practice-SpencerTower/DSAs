5d912c7846972d6dac2505f5525c89e8936f4299
SpencerTower <spencerjtower1@gmail.com>
2022-05-31 11:45:31 -0700

    adds remove method to singly linked list test file

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -29,17 +29,17 @@ class SinglyLinkedList {
         if (!this.head) return undefined;
         let current = this.head;
         let newTail = current;
-        while(current) {
+        while(current.next) {
             newTail = current;
             current = current.next;
-            }
+        }
         this.tail = newTail;
         this.tail.next = null;
-        this.length--;
         if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
+        this.length--;
         return current; // popped node
     }
     shift() {
@@ -47,11 +47,11 @@ class SinglyLinkedList {
         if (!this.head) return undefined;
         let shiftedNode = this.head;
         this.head = this.head.next;
-        this.length--;
         if (this.length === 0) {
             this.tail = null;
         }
         shiftedNode.next = null;
+        this.length--;
         return shiftedNode;
     }
     unshift(val) {
@@ -89,17 +89,39 @@ class SinglyLinkedList {
     insert(val, index) {
         let newNode = new Node(val);
         if (index < 0 || index > this.length) return false;
-        if (index === this.length) return !!this.push(val);
+        if (index === this.length - 1) return !!this.push(val);
         if (index === 0) return !!this.unshift(val);
 
         let previous = this.get(index - 1);
         newNode.next = previous.next;
         previous.next = newNode;
+        this.length++;
         return true;
+    }
+    remove(index) {
+        if (index < 0 || index > this.length) return undefined;
+        if (index === this.length - 1) return this.pop();
+        if (index === 0) return this.shift();
+
+        let previous = this.get(index - 1);
+        let removed = previous.next;
+        previous.next = removed.next;
+
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            let newTail = this.get(this.length - 2);
+            this.tail = newTail;
+        }
+        this.length--;
+        return removed;
     }
 }
 
 let newList = new SinglyLinkedList();
 newList.push("firstNode");
 newList.push("secondNode");
-console.log('list: ', newList);
+newList.push("thirdNode");
+newList.push("fourthNode");
+console.log('linked list: ', newList);
