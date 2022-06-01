bafdc7fc0066658ad2b29771d3fdf6e7b342d876
SpencerTower <spencerjtower1@gmail.com>
2022-06-01 11:26:18 -0700

    adds insert method to bst test code

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -13,11 +13,37 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+    insert (val) {
+        let newNode = new Node(val);
+        if (!this.root) this.root = newNode;
+        let current = this.root;
+        while (true) {
+            if (val === current.val) return undefined;
+            if (val < current.val) {
+                if (current.left) {
+                    current = current.left
+                } else {
+                    current.left = newNode;
+                    break;
+                }
+            }
+            if (val > current.val) {
+                if (current.right) {
+                    current = current.right;
+                } else {
+                    current.right = newNode;
+                    break;
+                }
+            }
+            break;
+        }
+        return this;
+    }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(15);
+tree.insert(7);
+tree.insert(9);
 console.log(treeify.asTree(tree, true));
