52cad2d8d25eb2d69ada93d69b9462a2f4c18d80
SpencerTower <spencerjtower1@gmail.com>
2022-07-03 13:11:53 -0700

    updates findMin solution code

--- code-challenges/arrays/min-in-rotated-sorted-array.js
+++ code-challenges/arrays/min-in-rotated-sorted-array.js
@@ -2,20 +2,19 @@
 
 const findMin = nums => {
     let min = nums[0];
-    let left = 0
+    let left = 0;
     let right = nums.length - 1;
     
     while (left <= right) {
-        if (nums[left] < nums[right]) {
+        if (nums[right] > nums[left]) {
             min = Math.min(min, nums[left]);
-            break;
         }
         let mid = Math.floor((left + right) / 2);
         min = Math.min(min, nums[mid]);
         if (nums[mid] >= nums[left]) {
             left = mid + 1;
         } else {
-            right = mid -1;
+            right = mid - 1;
         }
     }
     return min;
