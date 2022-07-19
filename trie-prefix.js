bbe63215813bc400c5a6842edc81215314494934
SpencerTower <spencerjtower1@gmail.com>
2022-07-19 12:35:13 -0700

    adds initial prefix trie solution

--- trie-prefix.js
+++ trie-prefix.js
@@ -3,23 +3,47 @@
 class TrieNode {
     constructor() {
         this.children = {};
-        this.endOfWord = "*";
+        this.endOfWord = false;
     }
 }
 
 class Trie {
     constructor() {
-        this.root = TrieNode();
+        this.root = new TrieNode();
     }
 
     insert(string) {
-
+        let node = this.root;
+        for (let char of string) {
+            if (!(char in node.children)) {
+                node.children[char] = new TrieNode(); // add character if doesnt exist
+            } else {
+                node = node.children[char]; // move to that character if already exists
+            }
+        }
+        node.endOfWord = true; // mark last character as end of word
     }
     search(string) {
-
+        let node = this.root;
+        for (let char of string) {
+            if (!(char in node.children)) {
+                return false; // word not in trie
+            }
+            node = node.children[char]; // go to next character
+        }
+        return node.endOfWord; // if last character is end, word exists in trie, returns true
     }
-    startsWith(string) {
-        
+    startsWith(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+            if (!(char in node.children)) {
+                return false; // word not in trie
+            }
+            node = node.children[char]; // go to next character
+        }
+        return true; // return true if any word has the prefix
     }
 }
 
+const trie = new Trie().insert('apple');
+console.log(trie.search('apple'));
--- trie-prefix.md
+++ trie-prefix.md
@@ -8,6 +8,8 @@ Tries:
   - for each character in the word
 - insert:
   - each character as a child of the previous character
+  - have to loop through each character in the word to be inserted
+  - check if character is in the children hashmap
   - have to mark the end of the word
 - search:
   - start at root
@@ -18,3 +20,11 @@ Tries:
   - start at root
   - look at child nodes, see if child nodes match
   - similar to search but doesn't have to be marked as end of word
+  - more efficient than using an array - with an array would have to loop through each word and check if it starts with the character
+  - O(1) lookup
+
+- Implementing a Trie
+  - Create a trie node
+  - Each node has children - can use a hashmap
+  - Each node has an end of word property - can be a boolean
+    - Can also use a character to mark the end of the word
