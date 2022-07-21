cc507f1ea0c222c83c30511641ec18347537a6a7
SpencerTower <spencerjtower1@gmail.com>
2022-07-21 15:03:15 -0700

    adds factorial recursion code

--- n-choose-k.js
+++ n-choose-k.js
@@ -1,9 +1,11 @@
 // N-Choose-K
 
+
 function nChooseK(n, k) {
     return factorialize(n) / (factorialize(k) * factorialize(n - k))
 }
 
+// For Loop
 function factorialize(num) {
 	if (num <= 1) return 1;
 	for (let i = num - 1; i >= 1; i--) {
@@ -13,3 +15,16 @@ return num;
 }
 
 console.log(nChooseK(7, 3))
+
+// Recursion
+function factorializeRec(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorializeRec(num - 1));
+    }
+  }
+
+console.log(factorializeRec(5));
