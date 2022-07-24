a7a7fdefefe72fa34ba285b5726200dd950d057c
SpencerTower <spencerjtower1@gmail.com>
2022-07-24 09:45:32 -0700

    adds lowest common ancestor solution code

---
+++ code-challenges/trees/lowest-common-ancestor.js
@@ -0,0 +1,15 @@
+// Lowest Common Ancestor
+
+function lowestCommonAncestor(root, p, q) {
+    let current = root;
+
+    while (current) {
+        if (p.val < current.val && q.val < current.val) {
+            current = current.left;
+        } else if (p.val > current.val && q.val > current.val) {
+            current = current.right;
+        } else {
+            return current;
+        }
+    }
+};
