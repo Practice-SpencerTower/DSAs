fc81982faef3680df007b75c2adaa4508594b583
SpencerTower <spencerjtower1@gmail.com>
2022-09-14 09:28:22 -0700

    adds optimized solution for sorted squared array

--- code-challenges/arrays/sorted-squared-array.js
+++ code-challenges/arrays/sorted-squared-array.js
@@ -10,3 +10,24 @@ function sortedSquaredArray(array) {
     squares.sort((a, b) => a - b);
     return squares;
 }
+
+// Time O(n) | Space O(n)
+function sortedSquaredArray(array) {
+    const squares = [];
+    let smallerIdx = 0;
+    let largerIdx = array.length - 1;
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        const smaller = array[smallerIdx];
+        const larger = array[largerIdx];
+
+        if (Math.abs(smaller) > Math.abs(larger)) {
+            squares[i] = smaller * smaller;
+            smallerIdx++;
+        } else {
+            squares[i] = larger * larger;
+            largerIdx--;
+        }
+    }
+    return squares;
+}
