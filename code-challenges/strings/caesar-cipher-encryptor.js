dbdbcff376562ed7b5a76b87bea4a25dce1edb3f
SpencerTower <spencerjtower1@gmail.com>
2022-11-15 15:34:42 -0800

    adds notes

---
+++ code-challenges/strings/caesar-cipher-encryptor.js
@@ -0,0 +1,12 @@
+// Caesar Cipher Encryptor - AlgoExpert
+
+// 26 letters in alphabet
+// modulus key in case its greater than 26
+// alpha keycode range is 97-122
+// loop through each letter in string
+// determine position in alphabet
+// add key to position
+// if position greater than 122, subtract by 26
+// determine letter at that number in alphabet
+// add new letter to output string
+// use charCodeAt and fromCharCode to convert letters to/from keycodes
