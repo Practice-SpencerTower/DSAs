370e2c2157b477d6f9ba13650eccbd7749963f50
SpencerTower <spencerjtower1@gmail.com>
2022-11-19 10:37:49 -0800

    adds generate document notes

--- code-challenges/strings/generate-document.js
+++ code-challenges/strings/generate-document.js
@@ -1,5 +1,10 @@
 // Generate Document - AlgoExpert
 
+// use object to track character count
+// loop through document and check if char in character object
+// if not in character object - document cannot be created, return false
+// if character in object, "use" character by decrementing character count
+
 // O(n + m) Time | Space O(n)
 function generateDocument(characters, document) {
     const charObj = {};
