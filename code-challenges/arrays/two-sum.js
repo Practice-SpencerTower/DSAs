17a9a268b23194e6c4bb8e2b4991c30110fafc52
SpencerTower <spencerjtower1@gmail.com>
2023-08-22 20:01:10 -0700

    adds two sum solution

--- code-challenges/arrays/two-sum.js
+++ code-challenges/arrays/two-sum.js
@@ -14,3 +14,19 @@ function twoNumberSum(array, targetSum) {
     return [];
 }
 
+function twoSum(nums, target) {
+    // using an object
+    const numObj = {};
+    let diff;
+    // find difference of current element and target
+    // check if difference in object
+    // make sure not same index as current element
+    for (let i = 0; i < nums.length; i++) {
+        diff = target - nums[i];
+        if (diff in numObj && numObj[diff] !== i) {
+            return [i, numObj[diff]];
+        }
+        // add current element to object, key = element, value = index
+        numObj[nums[i]] = i;
+    }
+}
