88f250a9d14ba99101c59e3e52d961555bde3378
SpencerTower <spencerjtower1@gmail.com>
2023-08-23 19:00:35 -0700

    adds optimization for containsDuplicate using set

--- code-challenges/arrays/contains-duplicate.js
+++ code-challenges/arrays/contains-duplicate.js
@@ -4,11 +4,36 @@
 // Return false if no duplicates
 
 // brute force
-for (let i = 0; i < nums.length; i++) {
-    let num1 = nums[i];
-    for (let j = i + 1; j < nums.length; j++) {
-        let num2 = nums[j];
-        if (num1 === num2) return true;
+function containsDuplicate(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let num1 = nums[i];
+        for (let j = i + 1; j < nums.length; j++) {
+            let num2 = nums[j];
+            if (num1 === num2) return true;
+        }
     }
+    return false;
 }
-return false;
+
+const input1 = [1, 2, 3, 1, 5, 6, 1, 7];
+console.log(
+    `input: ${input1} | contains duplicate? ${containsDuplicate(input1)}`
+);
+
+// using set
+// loop through, check if num in set
+// if in set, return true, if not in set, add to set
+function containsDuplicate2(nums) {
+    const set = new Set([]);
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (set.has(num)) return true;
+        set.add(num);
+    }
+    return false;
+}
+
+const input2 = [1, 2, 3, 1, 5, 6, 1, 7];
+console.log(
+    `input: ${input2} | contains duplicate? ${containsDuplicate2(input2)}`
+);
